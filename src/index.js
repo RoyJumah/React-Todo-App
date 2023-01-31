2690de65a6f468affead98741511f9b6b64a6e4d
_Roy Jumah <royjumah17@gmail.com>
2023-01-31 09:10:26 +0300

    Revert "Adding Features to React Todo app (upto hooks) "

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// component file
-import TodoContainer from './functionBased/components/TodoContainer';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// stylesheet
-import './functionBased/App.css';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
